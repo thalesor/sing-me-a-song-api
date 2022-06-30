829a2308e8abdb6c14a5a033cd5c39b20dfbc736
Marcus <marcusmoraes2010@hotmail.com>
2022-06-30 11:32:25 -0300

    Preparando revisao

--- tests/factories/recommendationFactory.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from "../../src/database.js";
-import { CreateRecommendationData } from "../../src/services/recommendationsService";
-
-export default async function recommendationFactory(recommendation: CreateRecommendationData[]) {
-  const recommendations = await prisma.recommendation.createMany({
-    data: [...recommendation]
-  });
-  return recommendations;
-}
